a90b18d3188ca19727015df0b74f8d0088dadd08
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2024-05-01 12:08:39 +0530

    chore: Create Bank related Schema

--- backend/db.js
+++ backend/db.js
@@ -39,6 +39,24 @@ module.exports = {
 	User
 };
 
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+
+module.exports = {
+	Account
+}
+
 
 
 
